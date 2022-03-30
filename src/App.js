4d954aee84027bca154648a8e0b6d15c7a92bc02
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-03-30 18:10:48 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const mobiles = [
+    {
+      model: "OnePlus 9 5G",
+      img: "https://m.media-amazon.com/images/I/61fy+u9uqPL._SX679_.jpg",
+      company: "Oneplus"
+    },
+    {
+      model: "Iphone 13 mini",
+      img:
+        "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-mini-blue-select-2021?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1645572315986",
+      company: "Apple"
+    },
+    {
+      model: "Samsung s21 ultra",
+      img: "https://m.media-amazon.com/images/I/81kfA-GtWwL._SY606_.jpg",
+      company: "Samsung"
+    },
+    {
+      model: "xiomi mi 11",
+      img: "https://m.media-amazon.com/images/I/51K4vNxMAhS._AC_SX522_.jpg",
+      company: "xiomi"
+    }
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {mobiles.map((mobile)=> <Phone mobile={mobile}/>)}
+      
+    </div>
+  );
+}
+
+function Phone({mobile}){
+  
+  return(
+    <div className="phone-container">
+      <img src={mobile.img} alt={mobile.model} className='phone-image'/>
+      <h2 className='phone-name'>{mobile.model}</h2>
+      <p className='phone-company'>{mobile.company}</p>
     </div>
   );
 }
